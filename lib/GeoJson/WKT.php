ba7930ac8a26919d4be3deabe4d093fd2e77eaf2
Filip Procházka <hosiplan@gmail.com>
2012-07-24 17:56:17 +0200

    refactoring; autoloading

--- lib/GeoJson/WKT.php
+++ lib/GeoJson/WKT.php
@@ -26,6 +26,18 @@ use Nette;
 class WKT extends Nette\Object
 {
 
+	const POINT = 'point';
+	const MULTIPOINT = 'multipoint';
+	const LINESTRING = 'linestring';
+	const MULTILINESTRING = 'multilinestring';
+	const LINEARRING = 'linearring';
+	const POLYGON = 'polygon';
+	const MULTIPOLYGON = 'multipolygon';
+	const GEOMETRYCOLLECTION = 'geometrycollection';
+
+	/**
+	 * @var array
+	 */
 	private $regExes = array(
 		'typeStr' => '/^\s*(\w+)\s*\(\s*(.*)\s*\)\s*$/',
 		'spaces' => '/\s+/',
@@ -34,22 +46,6 @@ class WKT extends Nette\Object
 		'trimParens' => '/^\s*\(?(.*?)\)?\s*$/'
 	);
 
-	const POINT = 'point';
-
-	const MULTIPOINT = 'multipoint';
-
-	const LINESTRING = 'linestring';
-
-	const MULTILINESTRING = 'multilinestring';
-
-	const LINEARRING = 'linearring';
-
-	const POLYGON = 'polygon';
-
-	const MULTIPOLYGON = 'multipolygon';
-
-	const GEOMETRYCOLLECTION = 'geometrycollection';
-
 
 
 	/**
@@ -57,13 +53,12 @@ class WKT extends Nette\Object
 	 *
 	 * @param string $WKT A WKT string
 	 *
-	 * @return Geometry|GeometryCollection
+	 * @return Geometry\Geometry|Geometry\GeometryCollection
 	 */
 	public function read($WKT)
 	{
-		$matches = array();
 		if (!preg_match($this->regExes['typeStr'], $WKT, $matches)) {
-			return null;
+			return NULL;
 		}
 
 		return $this->parse(strtolower($matches[1]), $matches[2]);
@@ -74,31 +69,31 @@ class WKT extends Nette\Object
 	/**
 	 * Parse WKT string into geometry objects
 	 *
-	 * @param string $WKT A WKT string
+	 * @param $type
+	 * @param $str
 	 *
-	 * @return Geometry|GeometryCollection
+	 * @return Geometry\Geometry|Geometry\GeometryCollection
 	 */
 	public function parse($type, $str)
 	{
-		$matches = array();
 		$components = array();
 
 		switch ($type) {
 			case self::POINT:
 				$coords = $this->pregExplode('spaces', $str);
-				return new Point($coords[0], $coords[1]);
+				return new Geometry\Point($coords[0], $coords[1]);
 
 			case self::MULTIPOINT:
 				foreach (explode(',', trim($str)) as $point) {
 					$components[] = $this->parse(self::POINT, $point);
 				}
-				return new MultiPoint($components);
+				return new Geometry\MultiPoint($components);
 
 			case self::LINESTRING:
 				foreach (explode(',', trim($str)) as $point) {
 					$components[] = $this->parse(self::POINT, $point);
 				}
-				return new LineString($components);
+				return new Geometry\LineString($components);
 
 			case self::MULTILINESTRING:
 				$lines = $this->pregExplode('parenComma', $str);
@@ -106,16 +101,16 @@ class WKT extends Nette\Object
 					$line = preg_replace($this->regExes['trimParens'], '$1', $l);
 					$components[] = $this->parse(self::LINESTRING, $line);
 				}
-				return new MultiLineString($components);
+				return new Geometry\MultiLineString($components);
 
 			case self::POLYGON:
 				$rings = $this->pregExplode('parenComma', $str);
 				foreach ($rings as $r) {
 					$ring = preg_replace($this->regExes['trimParens'], '$1', $r);
 					$linestring = $this->parse(self::LINESTRING, $ring);
-					$components[] = new LinearRing($linestring->getComponents());
+					$components[] = new Geometry\LinearRing($linestring->getComponents());
 				}
-				return new Polygon($components);
+				return new Geometry\Polygon($components);
 
 			case self::MULTIPOLYGON:
 				$polygons = $this->pregExplode('doubleParenComma', $str);
@@ -123,7 +118,7 @@ class WKT extends Nette\Object
 					$polygon = preg_replace($this->regExes['trimParens'], '$1', $p);
 					$components[] = $this->parse(self::POLYGON, $polygon);
 				}
-				return new MultiPolygon($components);
+				return new Geometry\MultiPolygon($components);
 
 			case self::GEOMETRYCOLLECTION:
 				$str = preg_replace('/,\s*([A-Za-z])/', '|$1', $str);
@@ -131,7 +126,7 @@ class WKT extends Nette\Object
 				foreach ($wktArray as $wkt) {
 					$components[] = $this->read($wkt);
 				}
-				return new GeometryCollection($components);
+				return new Geometry\GeometryCollection($components);
 
 			default:
 				return null;
@@ -143,6 +138,10 @@ class WKT extends Nette\Object
 	/**
 	 * Split string according to first match of passed regEx index of $regExes
 	 *
+	 * @param string $regEx
+	 * @param string $str
+	 *
+	 * @return array
 	 */
 	protected function pregExplode($regEx, $str)
 	{
@@ -156,11 +155,11 @@ class WKT extends Nette\Object
 	/**
 	 * Serialize geometries into a WKT string.
 	 *
-	 * @param Geometry $geometry
+	 * @param Geometry\Geometry $geometry
 	 *
 	 * @return string The WKT string representation of the input geometries
 	 */
-	public function write(Geometry $geometry)
+	public function write(Geometry\Geometry $geometry)
 	{
 		$type = strtolower(get_class($geometry));
 
@@ -176,16 +175,19 @@ class WKT extends Nette\Object
 	/**
 	 * Extract geometry to a WKT string
 	 *
-	 * @param Geometry $geometry A Geometry object
+	 * @param Geometry\Geometry $geometry A Geometry object
 	 *
-	 * @return strin
+	 * @return string
 	 */
-	public function extract(Geometry $geometry)
+	public function extract(Geometry\Geometry $geometry)
 	{
 		$array = array();
 		switch (strtolower(get_class($geometry))) {
 			case self::POINT:
-				return $geometry->getX() . ' ' . $geometry->getY();
+				/** @var Geometry\Point $geometry */
+				return number_format($geometry->getX(), 1, '.', '') . ' ' .
+					number_format($geometry->getY(), 1, '.', '');
+
 			case self::MULTIPOINT:
 			case self::LINESTRING:
 			case self::LINEARRING:
@@ -193,6 +195,7 @@ class WKT extends Nette\Object
 					$array[] = $this->extract($geom);
 				}
 				return implode(',', $array);
+
 			case self::MULTILINESTRING:
 			case self::POLYGON:
 			case self::MULTIPOLYGON:
@@ -200,11 +203,13 @@ class WKT extends Nette\Object
 					$array[] = '(' . $this->extract($geom) . ')';
 				}
 				return implode(',', $array);
+
 			case self::GEOMETRYCOLLECTION:
 				foreach ($geometry as $geom) {
 					$array[] = strtoupper(get_class($geom)) . '(' . $this->extract($geom) . ')';
 				}
 				return implode(',', $array);
+
 			default:
 				return null;
 		}
@@ -217,7 +222,7 @@ class WKT extends Nette\Object
 	 *
 	 * @param string $WKT
 	 *
-	 * @return  Geometry
+	 * @return Geometry\Geometry
 	 */
 	static public function load($WKT)
 	{
@@ -230,11 +235,11 @@ class WKT extends Nette\Object
 	/**
 	 * Dumps a Geometry Object into a  WKT string
 	 *
-	 * @param Geometry $geometry
+	 * @param Geometry\Geometry $geometry
 	 *
 	 * @return String A WKT string corresponding to passed object
 	 */
-	static public function dump(Geometry $geometry)
+	static public function dump(Geometry\Geometry $geometry)
 	{
 		$instance = new self;
 		return $instance->write($geometry);
